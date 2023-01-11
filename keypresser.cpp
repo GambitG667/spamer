2a9b461dfca5e2291f395b439cc73c1824b1297d
ArtymP <117117917+GambitG667@users.noreply.github.com>
2023-01-11 05:40:37 +0300

    добавил настройку задержки

--- keypresser.cpp
+++ keypresser.cpp
@@ -1,6 +1,7 @@
 #include <windows.h> //использует системные библиотеки такчто хрен его знает чем их заменить на линукс
 #include <WinUser.h>
 #include <locale.h>
+#include <time.h>
 
 
 void ctrl_v_enter() // сама симуляция нажатий. 
@@ -37,6 +38,8 @@ void ctrl_v_enter() // сама симуляция нажатий.
     ctrl_v[0].ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
     ctrl_v[1].ki.dwFlags = KEYEVENTF_KEYUP;
     SendInput(2, ctrl_v, sizeof(INPUT));
+    
+    Sleep(50); //Задержка нужна чтоб комп успевал переваривать нажатия, без неё он начинает лагать
 
     SendInput(1, &enter, sizeof(INPUT));
     enter.ki.dwFlags = KEYEVENTF_KEYUP;
@@ -60,43 +63,51 @@ void clibord(const char* text, int length) //функция загрузки в 
         }
 }
 
-int main()  //тут надо еще мучаться с задержкой и 6 сообщениями в очереди чтоб он не вставлял все в одно сообщение
+int main() 
 {
-    
+    srand(time(NULL));
+    int delay = 500;  // минимальная задержка между сообщениями. Очень сильно зависит от инета, задается вручную. У меня инет говно, так что я ставил на 1000.
+    int randomaiser = 100; //максимальная рандомная прибавка к задержке 100 - оптимально
+    int number_retry = 10; // количество повторов, до 20 все работает неплохо. потом дикорд тормозит и набивается очередь сообщений.
 
+    std::cout<<"input delay, randomaiser and retry: ";
+    std::cin>> delay >> randomaiser >> number_retry;
+
+    std::cout<<"OK. Wait 5 sec\n";
     Sleep(5000);
-    for(int i = 0; i < 20; ++i)
+    std::cout<<"Let's go";
+    for(int i = 0; i < number_retry; ++i)
     {
-        clibord("people are so weak", 19);
+
+        clibord(" https://tenor.com/view/fuhrer-cat-gas-gif-25955244\n", 53);
         ctrl_v_enter();
 
-        Sleep(50);
+        Sleep(delay + rand()%randomaiser); // рандомизация задержки
 
-
-        clibord("admin LOX", 10);
+        clibord(" https://tenor.com/view/kiss-gif-25984898\n", 43);
         ctrl_v_enter();
 
-        Sleep(50);
+        Sleep(delay + rand()%randomaiser);
 
-        clibord("https://tenor.com/view/fuhrer-cat-gas-gif-25955244", 51);
+        clibord(" https://tenor.com/view/felix-argyle-cute-smile-gif-13483002\n", 62);
         ctrl_v_enter();
 
-        Sleep(50);
+        Sleep(delay + rand()%randomaiser);
 
-        clibord("https://tenor.com/view/kiss-gif-25984898", 41);
+        clibord(" https://tenor.com/view/good-night-gif-23040113\n", 49);
         ctrl_v_enter();
 
-        Sleep(50);
+        Sleep(delay + rand()%randomaiser);
 
-        clibord("https://tenor.com/view/felix-argyle-cute-smile-gif-13483002", 60);
+        clibord("https://tenor.com/view/gachi-fist-gachimuchi-team-gif-21227013\n", 64);
         ctrl_v_enter();
 
-        Sleep(50);
+        Sleep(delay + rand()%randomaiser);
 
-        clibord("https://tenor.com/view/good-night-gif-23040113", 47);
+        clibord("https://tenor.com/view/%D0%BA%D1%83%D0%B7%D0%BD%D0%B5-%D1%81%D0%BF%D0%B8%D0%BB%D0%BB%D0%B5%D1%80-%D0%BA%D1%83%D0%B7%D0%BD%D0%B5%D1%83-%D0%BA%D1%83%D0%B7%D0%BD%D0%B5%D1%86-%D0%B3%D0%B5%D0%BD%D0%B0-gif-23203142\n", 210);
         ctrl_v_enter();
 
-        Sleep(50);
+        Sleep(delay + rand()%randomaiser);
         
     }
         
